3cfce60c1b6a7505d2533b1aed9192e5c1700a9f
NXH2801 <ngoxuanhinham123@gmail.com>
2025-03-30 01:42:19 +0700

    fix: route

--- src/router.js
+++ src/router.js
@@ -25,7 +25,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory('https://github.com/Belphegor2801/hanyu'),
+    history: createWebHistory(),
     routes,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'), // Adjust 'src' if your folder structure is different
+export default defineConfig(({ command, mode }) => {
+  // Determine the base path based on the environment
+  const base = mode === 'production' ? 'https://github.com/Belphegor2801/hanyu' : '/';
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'), // Adjust 'src' if your folder structure is different
+      },
     },
-  },
-})
+    base, // Set the base path
+  };
+});

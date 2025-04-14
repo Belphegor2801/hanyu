74834174e0d4876e42c5638307ad77bbf0e7b086
Belphegor2801 <ngoxuanhinham123@gmail.com>
2025-04-14 15:36:46 +0700

    feat: data bài 9

--- src/stores/mainStore.js
+++ src/stores/mainStore.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 
 export const useMainStore = defineStore('main', {
   state: () => ({
-    numOfLessons: 8
+    numOfLessons: 9
   }),
   actions: {
     setGlobalVariable(value) {
